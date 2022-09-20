3f2933425ea677132352979e2f401cacad574b5a
jeffbexes <96642253+jeffbexes@users.noreply.github.com>
2022-09-20 08:54:28 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,16 @@
-user_name= input("what is your name?")
-print ("hello", user_name, "welcome")
+fav_color= input ("what is your favorite color?")
+ndfavcolor= input ("what is the second best colour")
+thickness= input ("how thick do you like to draw?")
+print ("Good choices!")
 
 from cgitb import grey
 import turtle as trtl
 
 
 painter = trtl.Turtle()
-painter.fillcolor("red")
+painter.fillcolor(fav_color)
 painter.begin_fill()
-painter.pensize(3)
+painter.pensize(thickness)
 painter.forward(150)
 painter.right (90)
 painter.forward(120)
@@ -23,7 +25,7 @@ painter.right(180)
 painter.forward(30)
 painter.left(90)
 #2nd shape here
-painter.fillcolor("blue")
+painter.fillcolor(ndfavcolor)
 painter.begin_fill()
 painter.forward(30)
 painter.pendown()
@@ -36,6 +38,26 @@ painter.right(90)
 painter.forward(50)
 painter.end_fill()
 #ears begin hear if I have time
+painter.penup()
+painter.forward(30)
+painter.right(90)
+painter.pendown()
+painter.fillcolor(fav_color)
+painter.begin_fill()
+painter.circle(45)
+painter.end_fill()
+painter.penup()
+#2nd circle here
+painter.forward(90)
+painter.pendown()
+painter.fillcolor(fav_color)
+painter.begin_fill()
+painter.circle(45)
+painter.end_fill()
+
+
+
+
 
 wn = trtl.Screen()
 wn.mainloop()
