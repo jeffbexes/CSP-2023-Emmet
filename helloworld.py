c2a95140822600792bcad8d170bb46c8cf0fc466
jeffbexes <96642253+jeffbexes@users.noreply.github.com>
2022-10-04 08:54:33 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,63 +1,50 @@
-fav_color= input ("what is your favorite color?")
-ndfavcolor= input ("what is the second best colour")
-thickness= input ("how thick do you like to draw?")
-print ("Good choices!")
-
-from cgitb import grey
+#   a116_buggy_image.py
 import turtle as trtl
+#draw body
+spider = trtl.Turtle()
+spider.pensize(40)
+spider.circle(20)
+spider.speed(-1)
+#configure leg
+leg = 8
+length = 100
+angle = 180/ leg
+spider.pensize(5)
+leg_remain = 0
+#draw legs
+while (leg_remain < leg/2):
+  spider.goto(0,20)
+  spider.setheading(angle*leg_remain)
+  spider.forward(length)
+  leg_remain= leg_remain+ 1
+while (leg_remain<leg):
+  spider.goto(0,20) 
+  spider.setheading(angle*leg_remain-270)
+  spider.forward(length)
+  leg_remain= leg_remain+ 1
 
 
-painter = trtl.Turtle()
-painter.fillcolor(fav_color)
-painter.begin_fill()
-painter.pensize(thickness)
-painter.forward(150)
-painter.right (90)
-painter.forward(120)
-painter.right(90)
-painter.forward(150)
-painter.right(90)
-painter.forward(120)
-painter.end_fill()
-#painter.penup should be here
-painter.penup()
-painter.right(180)
-painter.forward(30)
-painter.left(90)
-#2nd shape here
-painter.fillcolor(ndfavcolor)
-painter.begin_fill()
-painter.forward(30)
-painter.pendown()
-painter.forward(90)
-painter.right(90)
-painter.forward(50)
-painter.right(90)
-painter.forward(90)
-painter.right(90)
-painter.forward(50)
-painter.end_fill()
-#ears begin hear if I have time
-painter.penup()
-painter.forward(30)
-painter.right(90)
-painter.pendown()
-painter.fillcolor(fav_color)
-painter.begin_fill()
-painter.circle(45)
-painter.end_fill()
-painter.penup()
-#2nd circle here
-painter.forward(90)
-painter.pendown()
-painter.fillcolor(fav_color)
-painter.begin_fill()
-painter.circle(45)
-painter.end_fill()
+spider.goto(0,20)
+spider.forward(15)
+spider.pencolor("red")
+spider.fillcolor("red")
+spider.begin_fill()
+spider.circle(5,-360)
+spider.end_fill()
+spider.penup()
+spider.goto(0,20)
+spider.left(70)
+spider.forward(15)
+spider.pendown()
+spider.pencolor("red")
+spider.fillcolor("red")
+spider.begin_fill()
+spider.circle(5,-360)
+spider.end_fill()
+spider.penup()
 
 
-
-
+spider.hideturtle()
 
 wn = trtl.Screen()
 wn.mainloop()
