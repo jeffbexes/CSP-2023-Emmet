6cec59ed4aee37f0777ad3fe3150f98c34101e18
jeffbexes <96642253+jeffbexes@users.noreply.github.com>
2022-10-11 09:52:54 -0700

    Update curvy spider.py

--- curvy spider.py
+++ curvy spider.py
@@ -1,54 +1,39 @@
-#a116_buggy_image.py
+#   a117_traversing_turtles.py
+#   Add code to make turtles move in a circle and change colors.
 import turtle as trtl
-#draw body
-spider = trtl.Turtle()
-spider.pensize(40)
-spider.circle(20)
-spider.speed(0)
-#configure leg
-leg = 8
 
-angle = 180/ leg
-spider.pensize(5)
-leg_remain = 0
-#draw legs
-while (leg_remain < leg/2):
-  spider.goto(0,20)
-  spider.pendown()
-  spider.setheading(angle*leg_remain)
-  spider.circle(100,100)
-  spider.penup()
-  leg_remain= leg_remain+ 1
-while (leg_remain<leg):
-  spider.goto(0,20) 
-  spider.pendown()
-  spider.setheading(angle*leg_remain-270)
-  spider.circle(-100,100)
-  spider.penup()
-  leg_remain= leg_remain+ 1
+# create an empty list of turtles
+my_turtles = []
+direction=40
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic","arrow", "turtle", "circle", "square", "triangle", "classic","arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold","red", "blue", "green", "orange", "purple", "gold","red", "blue", "green", "orange", "purple", "gold"]
 
+for s in turtle_shapes:
+  t = trtl.Turtle(shape=s)
+  new_color=turtle_colors.pop()
+  t.begin_fill()
+  t.fillcolor(new_color)
+  my_turtles.append(t)
+  t.end_fill()
 
-spider.goto(0,20)
-spider.forward(15)
-spider.pencolor("red")
-spider.fillcolor("red")
-spider.begin_fill()
-spider.circle(5,-360)
-spider.end_fill()
-spider.penup()
-spider.goto(0,20)
-spider.left(70)
-spider.forward(15)
-spider.pendown()
-spider.pencolor("red")
-spider.fillcolor("red")
-spider.begin_fill()
-spider.circle(5,-360)
-spider.end_fill()
-spider.penup()
+# go back to orgin
+startx = 0
+starty = 200
 
-
-spider.hideturtle()
+#the legs of the mobile
+for t in my_turtles:
+  t.penup()
+  t.goto(startx, starty)
+  t.setheading(direction)
+  t.pendown()     
+  t.right(20)
+  t.forward(50)
+  direction=t.heading()
+#the start positon for the next item
+  
+  startx = t.xcor()
+  starty = t.ycor()
 
 wn = trtl.Screen()
 wn.mainloop()
