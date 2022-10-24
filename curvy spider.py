039e8323eebdba69248837f318ce4236c9b6a329
jeffbexes <96642253+jeffbexes@users.noreply.github.com>
2022-10-24 09:54:39 -0700

    Update curvy spider.py

--- curvy spider.py
+++ curvy spider.py
@@ -1,39 +1,46 @@
-#   a117_traversing_turtles.py
-#   Add code to make turtles move in a circle and change colors.
+#setup
+import numbers
 import turtle as trtl
-
-# create an empty list of turtles
-my_turtles = []
-direction=40
-# use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic","arrow", "turtle", "circle", "square", "triangle", "classic","arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold","red", "blue", "green", "orange", "purple", "gold","red", "blue", "green", "orange", "purple", "gold"]
-
-for s in turtle_shapes:
-  t = trtl.Turtle(shape=s)
-  new_color=turtle_colors.pop()
-  t.begin_fill()
-  t.fillcolor(new_color)
-  my_turtles.append(t)
-  t.end_fill()
-
-# go back to orgin
-startx = 0
-starty = 200
-
-#the legs of the mobile
-for t in my_turtles:
-  t.penup()
-  t.goto(startx, starty)
-  t.setheading(direction)
-  t.pendown()     
-  t.right(20)
-  t.forward(50)
-  direction=t.heading()
-#the start positon for the next item
-  
-  startx = t.xcor()
-  starty = t.ycor()
-
+turtle=trtl
+import random as random
+#QOL changes
+trtl.hideturtle()
+trtl.speed(0)
+#calculations and inputs/outputs
+input1= input("How many zombies were there initaily")
+start= int(input1)
+input2= input("how fast do they replicate")
+replication=int(input2)
+input3= input("how many days have it been")
+days=int(input3)
+zombies= int(start**replication)
+zombozo= int(zombies*days)
+print("there are", zombozo, "zombies existing after", days,"days")
+#configure zomibe bodies
+radius=int(500/zombozo)
+#drawing the zombozos
+for step in range(zombozo):
+    trtl.penup()
+    for i in range (zombozo):
+        random_angles=random.randint(1,360)
+        random_distances=random.randrange(1,5)
+    trtl.pencolor("green")
+    trtl.fillcolor("green")
+    trtl.begin_fill()
+    for step in range (zombozo):
+        trtl.penup()
+        trtl.setheading(random_angles)
+        trtl.forward(random_distances)
+        trtl.pendown()
+    if zombozo<=9:
+     trtl.circle(radius)
+    elif zombozo >= 10:
+     trtl.circle(radius)
+    elif zombozo<=11:
+        trtl.circle(1)
+    trtl.penup()
+    trtl.goto(0,0)
+    trtl.pendown()
+    trtl.end_fill()
 wn = trtl.Screen()
 wn.mainloop()
